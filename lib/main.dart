82e4c904c75f68be18e32683668a022411167452
Murtaza <murtazamadasore@gmail.com>
2026-06-27 15:04:27 +0500

    Changed the starting to login screen

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Basic App",
-      home: HomePage(),
+      home: Loginpage()
     );
   }
 }
